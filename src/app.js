47681a26a92d9e664ab4f47337515c81bd223c01
unknown <lokeshsuwalka2002@gmail.com>
2025-09-16 14:57:52 +0530

    Playing with multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,19 +1,30 @@
 const express = require("express");
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Akshay", lastName: "Saini" });
-});
-app.post("/user/:id/:resid", (req, res) => {
-  //add the data to database
-  console.log("dynamic params", req.params);
-  console.log("query params", req.query);
-  res.send("data is added successfully");
-});
-app.delete("/user", (req, res) => {
-  //delete the data from database
-  res.send("deletion successfully");
-});
+app.use(
+  "/user/:id/:resID",
+  (req, res, next) => {
+    console.log("1st sir");
+    // res.send("1st response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("2nd sir");
+    console.log(req.params);
+
+    // res.send("2nd response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("3rd sir");
+    next();
+  },
+  (req, res, next) => {
+    res.send("4th response");
+    console.log(req.query);
+    next();
+  }
+);
 
 app.listen(3000, () => {
   console.log("service is running on port 3000...");
