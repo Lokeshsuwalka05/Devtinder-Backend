063c8f3b0634b597a336b0be3fb842f7a2051a73
unknown <lokeshsuwalka2002@gmail.com>
2025-09-16 14:59:32 +0530

    Playing with multiple round handlers

--- src/app.js
+++ src/app.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const app = express();
 
+// - Multiple route handler play with the code
+// - next()
+// - next function and errors along with the res.send()
+// - app.use("/route",rh1,[rh2,rh3],rh4)
 app.use(
   "/user/:id/:resID",
-  (req, res, next) => {
-    console.log("1st sir");
-    // res.send("1st response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("2nd sir");
-    console.log(req.params);
+  [
+    (req, res, next) => {
+      console.log("1st sir");
+      // res.send("1st response");
+      next();
+    },
+    (req, res, next) => {
+      console.log("2nd sir");
+      console.log(req.params);
 
-    // res.send("2nd response");
-    next();
-  },
+      // res.send("2nd response");
+      next();
+    },
+  ],
   (req, res, next) => {
     console.log("3rd sir");
     next();
