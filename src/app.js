845314d5a9fc07252761d654af0bfecf9a48747e
unknown <lokeshsuwalka2002@gmail.com>
2025-09-18 20:40:38 +0530

    CRUD Opeartion On User Model and Schema validation

--- src/app.js
+++ src/app.js
@@ -15,7 +15,69 @@ app.post("/signup", async (req, res) => {
       .send("Error while saving the data to the database" + " " + e.message);
   }
 });
-
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User Not Found");
+    } else {
+      res.send(users);
+    }
+  } catch (e) {
+    res.status(400).send("something went wrong");
+  }
+});
+//Feed API to get all the user
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(400).send("Something went wrong");
+  }
+});
+//Delete a user by id
+app.delete("/user", async (req, res) => {
+  const id = req.body.userId;
+  try {
+    await User.findByIdAndDelete(id);
+    res.send("User deleted successfully");
+  } catch (e) {
+    res.status(400).send("Something went wrong");
+  }
+});
+//update a user by id
+app.patch("/user", async (req, res) => {
+  const Id = req.body.userId;
+  const data = req.body;
+  console.log(Id, data);
+  try {
+    const user = await User.findByIdAndUpdate(Id, data, {
+      runValidators: true,
+    });
+    if (!user) {
+      res.status(401).send("User not found");
+    } else {
+      res.send("Update successfully");
+    }
+  } catch (e) {
+    res.status(400).send("Something went wrong" + e.message);
+  }
+});
+//update a user by Email
+app.patch("/userByEmail", async (req, res) => {
+  const emailId = req.body.emailId;
+  const data = req.body;
+  console.log(emailId, data);
+  try {
+    const user = await User.findOneAndUpdate({ emailId: emailId }, data);
+    console.log(user);
+    res.send("Update successfully");
+  } catch (e) {
+    res.status(400).send("Something went wrong");
+  }
+});
 connectDB()
   .then(() => {
     console.log("Database connection established....");
--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,56 @@
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  firstName: String, //shorthand for type:string
-  lastName: String,
-  emailId: String,
-  password: String,
-  age: Number,
-  gender: String,
-});
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 30,
+    },
+    lastName: {
+      type: String,
+      minLength: 2,
+      maxLength: 30,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate: {
+        validator: async (value) => {
+          const user = await User.findOne({ emailId: value });
+          if (user) {
+            return false;
+          } else {
+            return true;
+          }
+        },
+        message: "Email already exists",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "others"],
+    },
+    about: {
+      type: String,
+      default: "I will learn javascript for Sure",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 // creating user model by using userSchema
 const User = mongoose.model("User", userSchema);
 module.exports = { User };
