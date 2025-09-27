99bd59a7edc35051eeac5ec226e1aab735632794
Lokeshsuwalka05 <lokeshsuwalka2002@gmail.com>
2025-09-27 14:34:28 +0530

    Connection request pre Save function

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const connectionRequestSchema = new mongoose.Schema({
+  fromUserId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  toUserId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: {
+      values: ["interested", "ignored", "accepted", "rejected"],
+      message: "{VALUE} is not supported",
+    },
+  },
+});
+
+//connectionRequestSchema pre save function
+connectionRequestSchema.pre("save", function (next) {
+  const connectionRequest = this;
+  if (
+    connectionRequest.fromUserId.toString() ===
+    connectionRequest.toUserId.toString()
+  ) {
+    const err = new Error("Cannot send connection request to yourself");
+    return next(err);
+  }
+  next();
+});
+const ConnectionRequest = mongoose.model(
+  " ConnectionRequest",
+  connectionRequestSchema
+);
+module.exports = { ConnectionRequest };
