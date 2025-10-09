7905f93f06b332d0f30ecbb28a2c1bb6c479d7c3
Lokeshsuwalka05 <lokeshsuwalka2002@gmail.com>
2025-10-09 16:19:14 +0530

    Modified cors middleware

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const { userRouter } = require("./routes/user");
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 
