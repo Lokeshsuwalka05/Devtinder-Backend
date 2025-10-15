808c8fa117fc7576e216b7a5df65b4d0975f799c
Lokeshsuwalka05 <lokeshsuwalka2002@gmail.com>
2025-10-15 13:26:25 +0530

    Change the order for throwing an error

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -2,14 +2,14 @@ const validator = require("validator");
 const mongoose = require("mongoose");
 const validateSignupData = (data) => {
   const { firstName, lastName, emailId, password } = data;
-  if (!firstName) {
-    throw new Error("Invalid First Name");
-  } else if (!lastName) {
-    throw new Error("Invalid Last Name");
-  } else if (!validator.isEmail(emailId)) {
+  if (!validator.isEmail(emailId)) {
     throw new Error("Invalid Email");
   } else if (!validator.isStrongPassword(password)) {
     throw new Error("Please enter the strong password");
+  } else if (!firstName) {
+    throw new Error("Invalid First Name");
+  } else if (!lastName) {
+    throw new Error("Invalid Last Name");
   }
 };
 
